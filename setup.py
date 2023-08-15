5fe1f747c981cf15c8b50814b2bd6be158aa31cd
Zcaic <462845148@qq.com>
2023-08-15 19:56:48 +0800

    upload pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup,find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='FoilMesh',
     author='Zcaic',
     version='0.0.2',
     description='FoilMesh is a tool to mesh airfoil structural grid',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/Zcaic/Foilmesh",
     keywords="aerodynamics cfd airfoil grid mesh",
     packages=find_packages(),
